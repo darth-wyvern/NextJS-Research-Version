f68b5fd944802084ffe8a38d5ee18bca708bd6ac
darth-wyvern <nguyenquocnhan777@gmail.com>
2023-04-26 10:45:54 +0700

    move __test__

---
+++ __tests__/callApi.test.tsx
@@ -0,0 +1,19 @@
+import { render, screen, waitFor } from '@testing-library/react'
+import FetchData from '../src/components/Test/fetchAPI'
+import * as services from '../src/utils/Services'
+import React from 'react'
+
+test("Fetch Data API called", async () => {
+  const mockFetchData = jest.spyOn(services, 'FetchData')
+    .mockImplementation(async () => {
+      return [{
+        name: 'bulbasaur'
+      }];
+    })
+
+  render(<FetchData />)
+  expect(mockFetchData).toHaveBeenCalled();
+  await waitFor(() => {
+    expect(screen.getByText(/bulbasaur/i)).toBeInTheDocument();
+  })
+})
